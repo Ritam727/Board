78cb7fcd8fc1e794280d99626967c7cc6bd26b0d
Ritam727 <b20127@students.iitmandi.ac.in>
2021-10-22 21:27:36 +0530

    Initial Commit

---
+++ Paint.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2 as cv
+from cv2 import getTrackbarPos as gtp
+
+drawing = False
+brColor = [255, 255, 255]
+brushSize = 2
+
+def nothing(t):
+    pass
+
+def draw(event, x, y, flags, param):
+    global drawing, brColor, brushSize
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        drawing = True
+
+    elif event == cv.EVENT_MOUSEMOVE:
+        if drawing == True:
+            cv.circle(img, (x, y), brushSize, tuple(brColor), -1)
+
+    elif event == cv.EVENT_LBUTTONUP:
+        drawing = False
+        cv.circle(img, (x, y), brushSize, tuple(brColor), -1)
+
+img = np.zeros((720, 1280, 3), np.uint8)
+cv.namedWindow('Paint')
+cv.createTrackbar('B', 'Paint', 255, 255, nothing)
+cv.createTrackbar('G', 'Paint', 255, 255, nothing)
+cv.createTrackbar('R', 'Paint', 255, 255, nothing)
+cv.createTrackbar('Brush Size', 'Paint', 2, 20, nothing)
+cv.setMouseCallback('Paint', draw)
+
+while True:
+    cv.imshow('Paint', img)
+    k = cv.waitKey(1) & 0xFF
+    if k == 27:
+        break
+
+    brColor = [gtp('R', 'Paint'), gtp('G', 'Paint'), gtp('B', 'Paint')]
+    brushSize = gtp('Brush Size', 'Paint')
+
+cv.destroyAllWindows()
---
+++ README.md
@@ -0,0 +1,15 @@
+# Board
+
+A simple drawing board made with python and opencv. This project is incomplete, if someone wants to contribute, then feel free to fork this repo, clone the repo to your local machine, make changes and then make a Pull Request. Any help would be appreciated.
+
+If you want to use it, download the code as a zip file and then unzip it, or you can use git to clone it in your local machine.
+
+`$ git clone git@github.com:Ritam727/Board.git`
+
+To install the requirements, run the following command(make sure you have python3 and pip installed on your machine):
+
+`$ pip install -r requrements.txt`
+
+Then run the program with:
+
+`$ python main.py`
